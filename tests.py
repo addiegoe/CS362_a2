a966bf7099607dc09f84a60a6c0d2c33fb62dc2a
addiegoe <eaddiego@gmail.com>
2020-11-09 12:04:19 -0800

    test3 fails

--- tests.py
+++ tests.py
@@ -1,19 +1,23 @@
-# A2: TDD Testing hands-on
 # Author: Emily Addiego
 # Due Date: 11/9/20
 
 import unittest
 from check_pwd import check_pwd
 
-class TestCase(unittest.TestCase):
-  def test1(self):
-      input = ""
-      self.assertFalse(check_pwd(input))
 
-  def test2(self):
-      input = "hlhlfhglakfghklfadghffzgs"
-      self.assertFalse(check_pwd(input))
+class TestCase(unittest.TestCase):
+    def test1(self):
+        input = ""
+        self.assertFalse(check_pwd(input))
+
+    def test2(self):
+        input = "hlhlfhglakfghklfadghffzgs"
+        self.assertFalse(check_pwd(input))
+
+    def test3(self):
+        input = "BANANANA"
+        self.assertFalse(check_pwd(input))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
